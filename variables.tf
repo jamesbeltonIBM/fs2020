3feb59382fbaf12de04e7da45d5df11cc863f06c
James Belton <james.belton@uk.ibm.com>
2020-12-04 16:08:32 +0000

    Update variables.tf
    Changes for FS21 - just create an empty VPC

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "ibmcloud_region" {
 }
 
 variable "vpc_name" {
-  default = "vpc-fs2020-lab"
+  default = "vpc-fs2021-vpc-lab"
   description = "Name of your VPC"
 }
 
@@ -24,36 +24,16 @@ variable "zone3" {
 }
 
 variable "zone1_cidr" {
-  default = "172.16.1.0/24"
+  default = "10.10.0.0/24"
   description = "CIDR block to be used for zone 1"
 }
 
 variable "zone2_cidr" {
-  default = "172.16.2.0/24"
+  default = "10.20.0.0/24"
   description = "CIDR block to be used for zone 2"
 }
 
 variable "zone3_cidr" {
-  default = "172.16.3.0/24"
+  default = "10.30.0.0/24"
   description = "CIDR block to be used for zone 2"
-}
-
-variable "ssh_public_key" {
-  default = ""
-  description = "SSH Public Key contents to be used"
-}
-
-variable "image" {
-  default = "r006-e0039ab2-fcc8-11e9-8a36-6ffb6501dd33"
-  description = "OS Image ID to be used for virtual instances"
-}
-
-variable "profile" {
-  default = "cx2-2x4"
-  description = "Instance profile to be used for virtual instances"
-}
-
-variable "No_of_VSIs" {
-  default = "1"
-  description = "Number of VSIs to create on each subnet" 
 }
