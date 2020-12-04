2136b1a0ff5149a4749ee44ccacbee716b0e0600
James Belton <james.belton@uk.ibm.com>
2020-12-04 16:11:52 +0000

    Update main.tf
    changes for FS21

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-resource "ibm_is_ssh_key" "ssh1" {
-  name       = "ssh1"
-  public_key = "${var.ssh_public_key}"
-}
-
 resource "ibm_is_vpc" "vpc1" {
   name = "${var.vpc_name}"
 }
@@ -52,68 +47,8 @@ resource "ibm_is_subnet" "subnet3" {
   depends_on      = ["ibm_is_vpc_address_prefix.vpc-ap3"]
 }
 
-resource "ibm_is_instance" "instance1" {
-  count   = "${var.No_of_VSIs}"
-  name    = "instance1-${count.index}"
-  image   = "${var.image}"
-  profile = "${var.profile}"
-
-  primary_network_interface = {
-    subnet = "${ibm_is_subnet.subnet1.id}"
-  }
-  vpc  = "${ibm_is_vpc.vpc1.id}"
-  zone = "${var.zone1}"
-  keys = ["${ibm_is_ssh_key.ssh1.id}"]
-  user_data = "${data.template_cloudinit_config.cloud-init-apptier.rendered}"
-}
-
-resource "ibm_is_instance" "instance2" {
-  count   = "${var.No_of_VSIs}"
-  name    = "instance2-${count.index}"
-  image   = "${var.image}"
-  profile = "${var.profile}"
-
-  primary_network_interface = {
-    subnet = "${ibm_is_subnet.subnet2.id}"
-  }
-  vpc  = "${ibm_is_vpc.vpc1.id}"
-  zone = "${var.zone2}"
-  keys = ["${ibm_is_ssh_key.ssh1.id}"]
-  user_data = "${data.template_cloudinit_config.cloud-init-apptier.rendered}"
-}
-
-resource "ibm_is_instance" "instance3" {
-  count   = "${var.No_of_VSIs}"
-  name    = "instance1-${count.index}"
-  image   = "${var.image}"
-  profile = "${var.profile}"
-
-  primary_network_interface = {
-    subnet = "${ibm_is_subnet.subnet3.id}"
-  }
-  vpc  = "${ibm_is_vpc.vpc1.id}"
-  zone = "${var.zone3}"
-  keys = ["${ibm_is_ssh_key.ssh1.id}"]
-  user_data = "${data.template_cloudinit_config.cloud-init-apptier.rendered}"
-}
-
-resource "ibm_is_floating_ip" "floatingip1" {
-  name = "fip1"
-  target = "${ibm_is_instance.instance1-1.primary_network_interface.0.id}"
-}
-
-resource "ibm_is_floating_ip" "floatingip2" {
-  name = "fip2"
-  target = "${ibm_is_instance.instance2-1.primary_network_interface.0.id}"
-}
-
-resource "ibm_is_floating_ip" "floatingip3" {
-  name = "fip3"
-  target = "${ibm_is_instance.instance3-1.primary_network_interface.0.id}"
-}
 
 resource "ibm_is_security_group_rule" "sg1_tcp_rule_22" {
-  depends_on = ["ibm_is_floating_ip.floatingip1", "ibm_is_floating_ip.floatingip2"]
   group     = "${ibm_is_vpc.vpc1.default_security_group}"
   direction = "inbound"
   remote    = "0.0.0.0/0"
@@ -124,7 +59,6 @@ resource "ibm_is_security_group_rule" "sg1_tcp_rule_22" {
 }
 
 resource "ibm_is_security_group_rule" "sg1_tcp_rule_80" {
-  depends_on = ["ibm_is_floating_ip.floatingip1", "ibm_is_floating_ip.floatingip2"]
   group     = "${ibm_is_vpc.vpc1.default_security_group}"
   direction = "inbound"
   remote    = "0.0.0.0/0"
